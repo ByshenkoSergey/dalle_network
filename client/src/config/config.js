c1b2a87d51ea8b4b5cbde56657208d965415b8e3
Serhii Byshenko <byshenkoS1989@gmail.com>
2024-03-30 10:20:53 +0200

    Change server url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
 
-    backendUrl: "http://localhost:8080/api/v1"
+    backendUrl: "https://dalle-network.onrender.com/api/v1"
     
     // Please replase url if you want deploy into prodaction
-    // prodaction url: https://t-short-print-maker.onrender.com/api/v1/dalle
+    // prodaction url: https://dalle-network.onrender.com/api/v1
     // local url: http://localhost:8080/api/v1
   };
   
